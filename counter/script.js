e0ddb9d0052f3fde28bba19bc7a1bd0317c62915
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-10-15 09:53:08 +0530

    Conter Drill

---
+++ counter/script.js
@@ -0,0 +1,16 @@
+const input = document.querySelector("#input-value");
+const count = document.querySelector("#count");
+
+const incrementBtn = document.querySelector("#increment");
+const decrementBtn = document.querySelector("#decrement");
+
+incrementBtn.addEventListener("click", () => {
+  count.innerText = parseInt(count.innerText || 0) + parseInt(input.value || 0);
+});
+
+decrementBtn.addEventListener("click", () => {
+  if (parseInt(count.innerText) <= 0) {
+    return;
+  }
+  count.innerText = parseInt(count.innerText || 0) - parseInt(input.value || 0);
+});
