83e63c2998ba6d47055dfed0ff93a84378e55b52
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-10-15 16:11:08 +0530

    Add logic for disable button

--- transfer-list/script.js
+++ transfer-list/script.js
@@ -9,22 +9,33 @@ let secondBoxList = [
   { title: "Node", checked: false, id: 1232125 },
 ];
 
+const itemCount = { item1: 2, item2: 0 };
+
 function moveAllItemsToRight() {
   secondBoxList = [...secondBoxList, ...firstBoxList];
+  uncheckAllItems(secondBoxList);
   firstBoxList = [];
   render();
 }
 
 function moveAllItemsToLeft() {
   firstBoxList = [...firstBoxList, ...secondBoxList];
+  uncheckAllItems(firstBoxList);
   secondBoxList = [];
   render();
+}
+
+function uncheckAllItems(list) {
+  list.forEach((_, index, arr) => {
+    arr[index]["checked"] = false;
+  });
 }
 
 function moveItemsLeft() {
   const selectedItems = secondBoxList.filter(({ checked }) => checked === true);
   firstBoxList = [...firstBoxList, ...selectedItems];
   secondBoxList = secondBoxList.filter(({ checked }) => checked === false);
+  uncheckAllItems(firstBoxList);
   render();
 }
 
@@ -32,6 +43,7 @@ function moveItemsRight() {
   const selectedItems = firstBoxList.filter(({ checked }) => checked === true);
   secondBoxList = [...secondBoxList, ...selectedItems];
   firstBoxList = firstBoxList.filter(({ checked }) => checked === false);
+  uncheckAllItems(secondBoxList);
   render();
 }
 
@@ -69,6 +81,7 @@ function renderList(list, parent) {
 
     parent.appendChild(listItem);
   });
+  disableButton();
 }
 
 const allItemsRightBtn = document.querySelector("#all-right-btn");
@@ -86,8 +99,48 @@ moveRightBtn.addEventListener("click", moveItemsRight);
 function render() {
   const firstBoxContainer = document.querySelector("#first-box");
   const secondBoxContainer = document.querySelector("#second-box");
+  checkboxCount(firstBoxList, secondBoxList); // counting active checked items
   renderList(firstBoxList, firstBoxContainer);
   renderList(secondBoxList, secondBoxContainer);
+}
+
+function checkboxCount(list1, list2) {
+  itemCount["item1"] = 0;
+  itemCount["item2"] = 0;
+
+  list1.forEach(({ checked }) => {
+    if (checked) itemCount["item1"] += 1;
+  });
+
+  list2.forEach(({ checked }) => {
+    if (checked) itemCount["item2"] += 1;
+  });
+}
+
+function disableButton() {
+  if (firstBoxList.length === 0) {
+    allItemsRightBtn.setAttribute("disabled", "disabled");
+  } else {
+    allItemsRightBtn.removeAttribute("disabled");
+  }
+
+  if (secondBoxList.length === 0) {
+    allItemsLeftBtn.setAttribute("disabled", "disabled");
+  } else {
+    allItemsLeftBtn.removeAttribute("disabled");
+  }
+
+  if (itemCount["item1"] === 0) {
+    moveRightBtn.setAttribute("disabled", "disabled");
+  } else {
+    moveRightBtn.removeAttribute("disabled");
+  }
+
+  if (itemCount["item2"] === 0) {
+    moveLeftBtn.setAttribute("disabled", "disabled");
+  } else {
+    moveLeftBtn.removeAttribute("disabled");
+  }
 }
 
 render();
